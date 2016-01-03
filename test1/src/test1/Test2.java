499e8e8599cc8047723b30afc40681924cd64b41
zhangding <1993zd2014@gmail.com>
2016-01-03 20:56:31 +0800

    测试远程库与本地库库的合并

--- test1/src/test1/Test2.java
+++ test1/src/test1/Test2.java
@@ -2,11 +2,6 @@ package test1;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("测试EGit比较功能2");
-		System.out.println("好好好！看看会不会显示第二个提示push的提醒。。。");
-		System.out.println("好好好！看看ignore提示push的提醒。。。");
-		System.out.println("好好好！看看第二个ignore提示会不会提醒。。。");
-		System.out.println("好好好！看看修改后，不对文件使用ignore会不会有提示。。。");
-		System.out.println("好好好！看看重新断开后再连接，提交时会不会有提示。。。 ");
+		
 	}
 }
