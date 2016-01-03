539bb1fd0eb2ab552c61d70a5b89764aee1f94a1
zhangding <1993zd2014@gmail.com>
2016-01-03 18:24:10 +0800

    测试修改

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,7 +4,7 @@ package test1;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("这是测试git比较功能的测试");
-		System.out.println("哎呀！不行啊。看看在本地提交的时候可不可以啰。。。");
+		System.out.println("哎呀！不行啊。看看在本地提交的时候可不可以啰。。。 ");
 	}
 
 	
--- test1/src/test1/Test2.java
+++ test1/src/test1/Test2.java
@@ -7,6 +7,6 @@ public class Test2 {
 		System.out.println("好好好！看看ignore提示push的提醒。。。");
 		System.out.println("好好好！看看第二个ignore提示会不会提醒。。。");
 		System.out.println("好好好！看看修改后，不对文件使用ignore会不会有提示。。。");
-		System.out.println("好好好！看看重新断开后再连接，提交时会不会有提示。。。");
+		System.out.println("好好好！看看重新断开后再连接，提交时会不会有提示。。。 ");
 	}
 }
