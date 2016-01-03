b1fcca256081e6fbe9f00b180bd764fd0fea62ea
zhangding <1993zd2014@gmail.com>
2016-01-03 18:15:29 +0800

    好好好！看看ignore会不会提醒

--- test1/src/test1/Test2.java
+++ test1/src/test1/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("测试EGit比较功能2");
 		System.out.println("好好好！看看会不会显示第二个提示push的提醒。。。");
+		System.out.println("好好好！看看ignore提示push的提醒。。。");
 	}
 }
